81435f56254fdfb021e63c9b419822c2c8f6e00b
Ramon Ruiz <ramon.ruizperez@gmail.com>
2022-02-09 09:51:14 +0100

    modify test to use deferred utility

--- src/common/components/spinner/spinner.component.test.tsx
+++ src/common/components/spinner/spinner.component.test.tsx
@@ -8,46 +8,59 @@ import {
 import { SpinnerComponent } from './spinner.component';
 import { trackPromise } from 'react-promise-tracker';
 
-function createPromiseDelay({ delay = 100, willResolve = true } = {}) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      if (willResolve) resolve('promise resolved');
-      else reject('promise rejected');
-    }, delay);
+//utility to programatically resolve a promise
+function deferred() {
+  let resolve, rejected;
+  const promise = new Promise((res, rej) => {
+    resolve = res;
+    rejected = rej;
   });
+  return { promise, resolve, rejected };
 }
 
 describe('spinner.component', () => {
-  it('should not render modal before promise is created', () => {
+  it('should not render modal before promise if a promise is not provided to track Promise', () => {
     render(<SpinnerComponent />);
     expect(screen.queryByRole('presentation')).not.toBeInTheDocument();
   });
 
-  it('should show modal while promise is in progress', () => {
+  it('should show modal while promise is in progress', async () => {
+    const { promise, resolve } = deferred();
     render(<SpinnerComponent />);
+    let p;
     act(() => {
-      trackPromise(createPromiseDelay());
+      p = trackPromise(promise);
     });
-    screen.debug();
     expect(screen.getByRole('presentation')).toBeInTheDocument();
     expect(screen.getByRole('presentation')).toBeVisible();
+    //solve promise to trackPromise not affect rest of test
+    await act(async () => {
+      resolve();
+      await p;
+    });
   });
 
   it('modal should be removed after promise is resolved', async () => {
+    const { promise, resolve } = deferred();
     render(<SpinnerComponent />);
     act(() => {
-      trackPromise(createPromiseDelay());
+      trackPromise(promise);
     });
-
+    expect(screen.getByRole('presentation')).toBeInTheDocument();
+    expect(screen.getByRole('presentation')).toBeVisible();
+    resolve();
     await waitForElementToBeRemoved(() => screen.queryByRole('presentation'));
   });
 
   it('modal should be removed after promise is rejected', async () => {
+    const { promise, rejected } = deferred();
     render(<SpinnerComponent />);
     act(() => {
-      trackPromise(createPromiseDelay({ willResolve: false }));
+      trackPromise(promise);
     });
-
+    expect(screen.getByRole('presentation')).toBeInTheDocument();
+    expect(screen.getByRole('presentation')).toBeVisible();
+    rejected();
     await waitForElementToBeRemoved(() => screen.queryByRole('presentation'));
   });
 });
